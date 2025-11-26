8072a60a8afbd44b899aea50c92429e62699b126
stellarshenson <konrad.jelen@gmail.com>
2025-11-26 12:16:32 +0000

    chore: release v1.0.2 - prettify and prepare for release

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,13 @@ import {
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_fit_image_size_extension:plugin',
-  description: 'Jupyterlab extension to fit image to view size when image too large, and leave it as-is when smaller. This applies to all raster images (png, jpg etc..) and vector images (svg etc..)',
+  description:
+    'Jupyterlab extension to fit image to view size when image too large, and leave it as-is when smaller. This applies to all raster images (png, jpg etc..) and vector images (svg etc..)',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab_fit_image_size_extension is activated!');
+    console.log(
+      'JupyterLab extension jupyterlab_fit_image_size_extension is activated!'
+    );
   }
 };
 
